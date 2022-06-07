edfd6913511ac430809219b8384f2b0603b23140
MSAndromeda <ageoffcontent@gmail.com>
2022-06-07 09:34:04 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,70 @@
+console.clear();
+document.addEventListener('DOMContentLoaded', () => {
+
+    // Card options
+    const cardArray = [{
+            Name: 'fries',
+            img: 'Images/fries.png',
+        },
+        {
+            Name: 'fries',
+            img: 'Images/fries.png',
+        },
+        {
+            Name: 'Burger',
+            img: 'Images/Burger.png',
+        },
+        {
+            Name: 'Burger',
+            img: 'Images/Burger.png',
+        },
+        {
+            Name: 'hotdog',
+            img: 'Images/hotdog.png',
+        },
+        {
+            Name: 'hotdog',
+            img: 'Images/hotdog.png',
+        },
+        {
+            Name: 'ice-cream',
+            img: 'Images/ice-cream.png',
+        },
+        {
+            Name: 'ice-cream',
+            img: 'Images/ice-cream.png',
+        },
+        {
+            Name: 'milkshake',
+            img: 'Images/milkshake.png',
+        },
+        {
+            Name: 'milkshake',
+            img: 'Images/milkshake.png',
+        },
+        {
+            Name: 'pizza',
+            img: 'Images/pizza.png',
+        },
+        {
+            Name: 'pizza',
+            img: 'Images/pizza.png',
+        },
+        {
+            Name: 'white',
+            img: 'Images/white.png',
+        },
+        {
+            Name: 'white',
+            img: 'Images/white.png',
+        },
+        {
+            Name: 'blank',
+            img: 'Images/blank.png',
+        },
+        {
+            Name: 'blank',
+            img: 'Images/blank.png',
+        },
+    ]
+});
